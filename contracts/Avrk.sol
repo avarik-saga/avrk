3ec2d0c9e87b66756303e19ba6cee02ab76f9f5d
69hunter <kltan69@gmail.com>
2022-02-28 23:32:00 +0800

    fix: fix based on audit comment

--- contracts/Avrk.sol
+++ contracts/Avrk.sol
@@ -3,36 +3,19 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract Avrk is Ownable, ERC20Burnable, ERC20Pausable {
+contract Avrk is Ownable, ERC20Burnable {
     // Initial supply 500 000 000
-    uint256 private constant MAX_SUPPLY = 500000000e18;
-    bool private initialClaimed = false;
+    uint256 private constant MAX_SUPPLY = 5e8 ether;
+    bool private initialClaimed;
 
     constructor() ERC20("Avarik", "AVRK") {
         _mint(address(this), MAX_SUPPLY);
     }
 
-    function claimTokens() public onlyOwner {
-        require(initialClaimed == false, "AVRK: Has claimed");
+    function claimTokens() external onlyOwner {
+        require(!initialClaimed, "AVRK: Has claimed");
         initialClaimed = true;
         _transfer(address(this), _msgSender(), MAX_SUPPLY);
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20, ERC20Pausable) {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
